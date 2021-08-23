85d21f436061b4a68c3e7c95ed2980741eebf73a
Paul <paulxcz@gmail.com>
2021-08-23 02:13:23 -0500

    docs agregado

---
+++ docs/precache-manifest.655eba985a06b7539c6c1f1d0e6f50a1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fb0f7bf5e27f9e9bffd24e81fcffe28",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c43815f92e1c85a5bbdb",
+    "url": "/static/css/main.832776f2.chunk.css"
+  },
+  {
+    "revision": "8c639a6bff16b54b9f77",
+    "url": "/static/js/2.629c55af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.629c55af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c43815f92e1c85a5bbdb",
+    "url": "/static/js/main.b4a4fc8c.chunk.js"
+  },
+  {
+    "revision": "5bba4b9ed69c6568a70e",
+    "url": "/static/js/runtime-main.95118175.js"
+  }
+]);
